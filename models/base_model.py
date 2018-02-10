ad1509e61bb7d4947a410a044814a4ee56338062
vkalia602 <vkalia602@gmail.com>
2018-02-10 15:13:50 +0000

    creates BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -11,10 +11,17 @@ class BaseModel():
     class BaseModel
     """
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = (datetime.now())
-        self.updated_at = (datetime.now())
+    def __init__(self, *args, **kwargs):
+        if len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key in ["created_at", "updated_at"]:
+                   value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if '__class__' not in key:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = (datetime.now())
+            self.updated_at = (datetime.now())
 
     def __str__(self):
         """

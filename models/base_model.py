2683d56258d21b02862893281781e8883c29c264
vkalia602 <vkalia602@gmail.com>
2018-02-11 22:12:05 +0000

    updated to load from json file and create an instance

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,7 @@ Module that defines all common attributes and models for base class
 
 from datetime import datetime
 import uuid
-from models import storage
+import models
 class BaseModel():
     """
     class BaseModel
@@ -23,7 +23,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = (datetime.now())
             self.updated_at = (datetime.now())
-            storage.new(self)        
+            models.storage.new(self)        
     def __str__(self):
         """
         Method that returns string representation on an instance
@@ -38,6 +38,7 @@ class BaseModel():
         """
 
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,6 +5,7 @@ Module that serializes instances to JSON file
 
 import json
 import os
+from models.base_model import BaseModel
 class FileStorage():
     """
     Class FileStorage to serialize/deserialize a JSON file
@@ -16,16 +17,22 @@ class FileStorage():
 
     def new(self, obj):
         x = ("{}.{}".format(obj.__class__.__name__, obj.id))
-        self.__objects[x] = obj.to_dict
+        print(obj)
+        self.__objects[x] = obj
 
     def save(self):
-        with open(self.__file_path, mode="w", encoding="UTF-8") as my_file:
-            json.dump(self.__objects, my_file)
+        new_dict = {}
+        for key, value in self.__objects.items():
+            new_dict[key] = value.to_dict()
+        with open(self.__file_path, mode="w+", encoding="UTF-8") as my_file:
+            json.dump(new_dict, my_file)
 
     def reload(self):
-        if os.path.isfile(self.__file_path) and os.stat(self.__file_path).st_size != 0:
-            with open(self.__file_path, mode='r', encoding="UTF-8") as my_file:
-               self.__objects = json.load(my_file)
-        else:
-            pass
-            
+        if os.path.isfile(self.__file_path):
+            if os.stat(self.__file_path).st_size != 0:
+                with open(self.__file_path, mode='r', encoding="UTF-8") as my_file:
+                    new_object = json.load(my_file)
+            for key, value in new_object.items():
+                class_name = value['__class__']
+                self.__objects[key] = eval(class_name)(**value)
+                

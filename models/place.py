06d4bd29df1ab2c73e760bb689dbd5eefa0662e8
Lee Gaines <eightlimbed@gmail.com>
2018-02-14 21:07:42 +0000

    changed init method of Place models

--- models/place.py
+++ models/place.py
@@ -31,6 +31,18 @@ class Place(BaseModel):
     Place: class for Place models. inherits from BaseModel
     """
 
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
+
     place_attrs = {'city_id', 'user_id', 'name', 'description',
                    'number_rooms', 'number_bathrooms', 'max_guest',
                    'price_by_night', 'latitude', 'longitude', 'amenity_ids'}
@@ -46,14 +58,14 @@ class Place(BaseModel):
                 if key in Place.place_attrs:
                     self.name = val
         else:
-            self.city_id = ""
-            self.user_id = ""
-            self.name = ""
-            self.description = ""
-            self.number_rooms = 0
-            self.number_bathrooms = 0
-            self.max_guest = 0
-            self.price_by_night = 0
-            self.latitude = 0.0
-            self.longitude = 0.0
-            self.amenity_ids = []
+            self.city_id = Place.city_id
+            self.user_id = Place.user_id
+            self.name = Place.name
+            self.description = Place.description
+            self.number_rooms = Place.number_rooms
+            self.number_bathrooms = Place.number_bathrooms
+            self.max_guest = Place.max_guest
+            self.price_by_night = Place.price_by_night
+            self.latitude = Place.latitude
+            self.longitude = Place.longitude
+            self.amenity_ids = Place.amenity_ids

750ea0a208af37f2161f8cd88033accc9000d306
Lee Gaines <eightlimbed@gmail.com>
2018-02-14 06:09:25 +0000

    fixes User attribute setting issue

--- models/user.py
+++ models/user.py
@@ -17,8 +17,19 @@ class User(BaseModel):
     def __init__(self, *args, **kwargs):
         """User constructor, inheriting id, created_at, updated_at
         from BaseModel"""
-        super().__init__()
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
+        super().__init__(self, *args, **kwargs)
+        if kwargs:
+            for key, val in kwargs.items():
+                if key == 'email':
+                    self.email = val
+                elif key == 'password':
+                    self.password = val
+                elif key == 'first_name':
+                    self.first_name = val
+                elif key == 'last_name':
+                    self.last_name = val
+        else:
+            self.email = ""
+            self.password = ""
+            self.first_name = ""
+            self.last_name = ""
